0d791131935ae2fe15c6036d7d362a15a6112b74
guyifeng <santree122@gmail.com>
2024-03-22 16:02:29 +0800

    fix: 去除敏感信息

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export default {
-    dynamicIpsApi: 'http://list.sky-ip.net/user_get_ip_list?token=QpTWKtsow2SD6h4R1709625840896&type=datacenter&qty=5&country=my&time=10&format=json&protocol=http&area=sg',
+    dynamicIpsApi: '',
     walletPassword: 'iloveme123',
     headless: false,
     parallelCount: 1,
-    "2captchaToken": "00a34b59d0d78ecb3dac10db016c7c50",
+    "2captchaToken": "",
 }
